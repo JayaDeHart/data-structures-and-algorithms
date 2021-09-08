3b5d0218b1347eb5361eca6610ae6347f59bb6e7
Jaya DeHart <dehartj97@gmail.com>
2021-09-08 09:08:23 -0700

    ready to submit

--- trees/__test__/binary-tree.test.js
+++ trees/__test__/binary-tree.test.js
@@ -22,4 +22,19 @@ describe("Binary Tree", () => {
     let output = bt.traverse_pre();
     expect(output).toEqual([10, 15, 100, 1]);
   });
+
+  it("should be able to find the max", () => {
+    let tree = {
+      value: 10,
+      left: {
+        value: 15,
+        left: null,
+        right: { value: 100, left: null, right: null },
+      },
+      right: { value: 1, left: null, right: null },
+    };
+    let bt = new BinaryTree(tree);
+    let max = bt.findMax();
+    expect(max).toBe(100);
+  });
 });
